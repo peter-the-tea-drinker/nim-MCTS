17355df664188234a246cc869ca1f9c90afee195
Peter Row <peter.row@gmail.com>
2014-12-04 12:44:54 +1100

    Added license

--- nimMCTS.nim
+++ nimMCTS.nim
@@ -1,3 +1,37 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Peter James Row
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# MPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+##########################################################################################################
+
+# Based on UCT Monte Carlo Tree Search algorithm in Python 2.7.
+
+# Written by Peter Cowling, Ed Powley, Daniel Whitehouse (University of York, UK) September 2012.
+
+# Licence is granted to freely use and distribute for any sensible/legal purpose so long as this comment
+# remains in any distributed code.
+
+# For more information about Monte Carlo Tree Search check out our web site at www.mcts.ai
+
+
 import algorithm
 import math
 
